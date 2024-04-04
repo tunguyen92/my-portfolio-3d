382508bee871cf1fe0325b0e52f2043b0c5b84f0
Kantee <tunguyen3692@gmail.com>
2024-04-04 17:27:26 +0700

    Created HomePage animation

---
+++ src/models/Plane.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+
+import planeScene from "~/assets/3d/plane.glb";
+
+const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
+  const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+  }, [actions, isRotating]);
+
+  return (
+    <mesh {...props} ref={ref}>
+      {/* use the primitive element when you want to directly embed a complex 3D model or scene */}
+      <primitive object={scene} />
+    </mesh>
+  );
+};
+
+export default Plane;
---
+++ src/models/Sky.jsx
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+import skyScene from "~/assets/3d/sky.glb";
+
+const Sky = ({ isRotating }) => {
+  const sky = useGLTF(skyScene);
+  const skyRef = useRef();
+
+  // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
+  // It ensures smooth animations by making the rotation frame rate-independent.
+  // 'delta' represents the time in seconds since the last frame.
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.5 * delta; // Adjust the rotation speed as needed
+    }
+  });
+
+  return (
+    <mesh ref={skyRef}>
+      {/* use the primitive element when you want to directly embed a complex 3D model or scene */}
+      <primitive object={sky.scene} />
+    </mesh>
+  );
+};
+
+export default Sky;
